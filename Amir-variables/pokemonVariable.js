62d206d358ad3dd95030548b367a3a7c5b86b170
AmirASC4 <amirw4531@kippnyccp.org>
2017-07-11 10:09:35 -0700

    first commit

---
+++ Amir-variables/pokemonVariable.js
@@ -0,0 +1,16 @@
+var garchomphp=170;
+var garchompattack= 190;
+var garchompdefense=150;
+var garchomptype="Dragon,Ground";
+var garchompbestmove="Draco meteor";
+var garchompspeed= 102;
+var garchompspatk=80;
+var garchompspdef=85;
+var garchomplastevo= true;
+
+var lucariohp=70;
+var lucarioattack=110;
+var lucariodefense=70;
+var lucariotype="steel, Fighting"
+var lucariobestmove=""
+console.log(lucarioattack)
---
+++ pokemonArray.js
@@ -0,0 +1,17 @@
+var myPokemonRoster= ["pikachu","charizard","Blastoise",'swellow'];
+
+var firstToFight = myPokemonRoster[0];
+var secondtofight = myPokemonRoster[1];
+var Thirdtofight = myPokemonRoster[2];
+var forthtofight = myPokemonRoster[3];
+
+
+for (var i=0; i<myPokemonRoster.length; i ++){
+    console.log(myPokemonRoster[i])
+}
+
+
+for (var i=0; i<myPokemonRoster.length; i ++){
+    var a=myPokemonRoster.length-i-1
+    console.log(myPokemonRoster[a])
+}
